134f01bd65ba1d4a2b502170f57fe80d6f6cf899
m-lutter <116616446+m-lutter@users.noreply.github.com>
2024-10-22 17:50:26 -0500

    Update 5421BCD.cpp

--- Circuit 1: 5421 BCD/5421BCD.cpp
+++ Circuit 1: 5421 BCD/5421BCD.cpp
@@ -1,18 +1,13 @@
 // Maxwell Lutter
-// very simple testing program for 5421 BCD
-// prints out the truth table and one test expression
-
+// Simple truth table generator
 #include <iostream>
 
-int main() {
-    std::cout << "\tABCD   a b c d e f g test" << std::endl;
-    std::cout << "\t-------------------------" << std::endl;
-    
-    bool a, b, c, d, fa, fb, fc, fd, ff, fe, fg, test;
-    
-    for (int i = 0; i <= 15; i++) {
-        // first generating the input values
-        if (i < 8) {
+int main()
+{
+  bool a, b , c, d, f;
+  for (int i = 0; i < 16; i++) {
+      // generating the input values
+      if (i < 8) {
             a = 0;
         } else {
             a = 1;
@@ -24,25 +19,8 @@ int main() {
         }
         c = i % 4 / 2;
         d = i % 2;
-        
-        // ignoring the don't care terms
-        if (i > 7 && i != 11 && i != 12) {
-            std::cout << '\t' << a << b << c << d << " | x x x x x x x x" << std::endl;
-            continue;
-        }
-        
-        // finding the value of each expression
-        fa = a || c || (b == d);
-        fb = !b || (c == d);
-        fc = (b || !c || d);
-        fd = (!b != (!c && d)) || (c && !d);
-        fe = (!d || (a && c)) && (!b || c);
-        ff = a || (b && (!c || !d)) || (!c && !d);
-        fg = (b || c) && (!b || !c || !d);
-        test = (b && !c) || (b && !d) || (c && !d) || (!b && c); // example g
-        
-        // print the result of each sequence of inputs
-        std::cout << '\t' << a << b << c << d << " | " << fa << " " << fb  << " " << fc << " " << fd << " " << fe << " " << ff << " " << fg  << " " << test << std::endl;
-        return 0;
-    }
+        // put test expression below
+        f = (a && !b && d) || (!a && !d && c) || (!a && b && !c && d) || (a && !c && !d) || (!b && c && d) || (!a && !b && !d);
+        std::cout << f << std::endl;
+  }
 }
